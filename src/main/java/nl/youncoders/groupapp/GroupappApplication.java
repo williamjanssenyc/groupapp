0798ba67369401175e47e6f8b6060d229531627b
Kevin van der Linde <kevin.vanderlinde@youngcoders.nl>
2018-04-19 11:24:55 +0200

    Initial commit

---
+++ src/main/java/nl/youncoders/groupapp/GroupappApplication.java
@@ -0,0 +1,12 @@
+package nl.youncoders.groupapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupappApplication.class, args);
+	}
+}
