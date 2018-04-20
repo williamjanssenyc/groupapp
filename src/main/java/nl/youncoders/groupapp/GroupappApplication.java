2b04480eb0d595edbec132e61a2aca4fbdc89977
Kevin van der Linde <kevin.vanderlinde@youngcoders.nl>
2018-04-20 15:43:40 +0200

    Add a TestServlet object that gets mapped to both /index and /foo.

--- src/main/java/nl/youncoders/groupapp/GroupappApplication.java
+++ src/main/java/nl/youncoders/groupapp/GroupappApplication.java
@@ -2,11 +2,23 @@ package nl.youncoders.groupapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class GroupappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GroupappApplication.class, args);
+	}
+
+	@Bean
+	ServletRegistrationBean myServletRegistration () {
+		ServletRegistrationBean srb = new ServletRegistrationBean();
+		srb.setServlet(new TestServlet());
+		srb.setUrlMappings(Arrays.asList("/index", "/foo"));
+		return srb;
 	}
 }
---
+++ src/main/java/nl/youncoders/groupapp/TestServlet.java
@@ -0,0 +1,19 @@
+package nl.youncoders.groupapp;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//@WebServlet(name = "AnnotationExample", description = "Example Servlet Using Annotations", urlPatterns = {"/index"})
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        out.println("Hello World, this is TestServlet speaking!");
+    }
+}
