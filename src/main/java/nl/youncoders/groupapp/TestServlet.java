1e21112c1357da4d09ca9d46fa6fc70e077df466
Kevin van der Linde <kevin.vanderlinde@youngcoders.nl>
2018-04-22 20:11:07 +0200

    Add a Controller to handle the root requests.

--- src/main/java/nl/youncoders/groupapp/TestServlet.java
+++ src/main/java/nl/youncoders/groupapp/TestServlet.java
@@ -1,11 +1,14 @@
 package nl.youncoders.groupapp;
 
+import org.springframework.context.annotation.Configuration;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@Configuration
 public class TestServlet extends HttpServlet {
 
     @Override
