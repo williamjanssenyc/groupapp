aa091774e066c110311aa245e595a316a4498f8b
Kevin van der Linde <kevin.vanderlinde@youngcoders.nl>
2018-04-22 18:57:56 +0200

    Removed Annotations that is not used and fixed some imports.

--- src/main/java/nl/youncoders/groupapp/TestServlet.java
+++ src/main/java/nl/youncoders/groupapp/TestServlet.java
@@ -1,19 +1,17 @@
 package nl.youncoders.groupapp;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-//@WebServlet(name = "AnnotationExample", description = "Example Servlet Using Annotations", urlPatterns = {"/index"})
 public class TestServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("Hello World, this is TestServlet speaking!");
+        out.println("<p>Hello World, this is TestServlet speaking!</p>");
     }
 }
